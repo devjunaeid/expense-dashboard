b9ba8f3ae7f6fad80fdc8323c1dae53ebb9170b6
junaeid <junaeid.md48@gmail.com>
2024-07-08 01:18:36 +0600

    Added a small massage with Dashboard link.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,14 @@
 <script setup>
+import { RouterLink } from 'vue-router';
+
 </script>
 
 <template>
-  <main>
+  <main class="w-full h-screen flex justify-center items-center">
+   <div class="flex flex-col gap-4 text-black text-center font-poppins text-2xl">
+    <p>Welcome! to Expense Dashbord Homepage</p>
+    <p class="font-bold text-xs">NB: Designed for desktop mode only (1920x1080)</p>
+    <RouterLink class="bg-green-400 text-green-950 font-bold p-4" to="/dashboard/expense">Open Dashbord</RouterLink>
+   </div> 
   </main>
 </template>
