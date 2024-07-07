9d5674e5ba92858a1fa56ab5a7331b88aeb16941
junaeid <junaeid.md48@gmail.com>
2024-07-07 13:19:28 +0600

    Added: profile component of nav and fixed nav item positions.

--- src/views/dashboard/DashboardView.vue
+++ src/views/dashboard/DashboardView.vue
@@ -1,11 +1,15 @@
 <script setup>
+import Profile from "@/components/Dashboard/nav/Profile.vue";
 import Links from "../../components/Dashboard/nav/Links.vue";
 </script>
 
 <template>
   <div class="w-screen h-screen flex items-center bg-black p-5">
-    <nav class="w-[350px] h-full">
+    <nav class="w-[350px] h-full flex flex-col justify-center items-center">
+     <div class="w-2/3 m-auto flex flex-col justify-start items-center">
+      <Profile/> 
       <Links />
+     </div> 
     </nav>
     <div class="flex-1 h-[90%] mt-6 rounded-lg bg-white">
       <RouterView />
