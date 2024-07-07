57d20a0eba88ba0edab88954bd6df0d7aac8eb00
junaeid <junaeid.md48@gmail.com>
2024-07-08 00:31:41 +0600

    Added: Code refector and added comments.

--- src/views/dashboard/DashboardView.vue
+++ src/views/dashboard/DashboardView.vue
@@ -4,14 +4,18 @@ import Links from "../../components/Dashboard/nav/Links.vue";
 </script>
 
 <template>
-  <div class="w-screen h-screen flex items-center bg-black p-5">
-    <nav class="w-[350px] h-full flex flex-col justify-center items-center">
-     <div class="w-2/3 m-auto flex flex-col justify-start items-center">
-      <Profile/> 
-      <Links />
-     </div> 
+  <div class="w-screen h-screen flex items-center bg-black p-6">
+   
+    <!-- Dashboard Nav -->
+    <nav class="w-[300px] h-full flex flex-col justify-center items-center">
+      <div class="w-2/3 m-auto flex flex-col justify-start items-center">
+        <Profile />
+        <Links />
+      </div>
     </nav>
-    <div class="flex-1 h-[90%] mt-6 rounded-lg bg-white">
+    
+    <!-- SubRoute/Component View -->
+    <div class="flex-1 h-[90%] m-auto rounded-2xl bg-white">
       <RouterView />
     </div>
   </div>
