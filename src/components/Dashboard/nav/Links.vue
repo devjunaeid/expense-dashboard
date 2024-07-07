9fbcab989e7ee76d1b8793b7bf99f33439d50a0f
junaeid <junaeid.md48@gmail.com>
2024-07-07 13:18:09 +0600

    Added: Acive link styling and defining other nav links.

--- src/components/Dashboard/nav/Links.vue
+++ src/components/Dashboard/nav/Links.vue
@@ -1,23 +1,39 @@
 <script setup>
 import { ref } from 'vue';
 import { RouterLink } from 'vue-router';
+
+// All nevigation link definition.
 const links = ref(
   [
-    { id: 1, title: "Expense", url: "/dashboard/expense" },
-    { id: 1, title: "Expense", url: "/dashboard/expense" }
+    { id: 1, title: "Dashboard", url: "/dashboard" },
+    { id: 2, title: "Expense", url: "/dashboard/expense" },
+    { id: 3, title: "Wallets", url: "/dashboard/walltes" },
+    { id: 4, title: "Summary", url: "/dashboard/summary" },
+    { id: 5, title: "Accounts", url: "/dashboard/accounts" },
+    { id: 6, title: "Settings", url: "/dashboard/settings" }
   ]
 ) 
 </script>
 
 <template>
-  <div class="w-full flex flex-col justify-start align-middle" v-for="(link, index) in links" :key="link.id">
-    
-   <RouterLink class="text-2xl text-white font-semibold font-poppins" :to="link.url">{{ link.title }}</RouterLink>
+
+  <!-- Iterate over links array -->
+  <div class="w-full h-fit flex flex-col justify-start align-middle" v-for="(link, index) in links" :key="link.id">
+
+    <!-- Checking if the current url is the active url -->
+    <div v-if="$route.path == link.url" class="py-4">
+      <RouterLink class="text-[25px] text-white font-semibold font-poppins" :to="link.url">{{ link.title }}
+      </RouterLink>
+    </div>
+
+    <div v-else class="py-4">
+      <RouterLink class="text-[25px] text-gray-500 font-semibold font-poppins" :to="link.url">{{ link.title }}
+      </RouterLink>
+    </div>
   </div>
 </template>
 
 <style scoped>
-
 .list-cont {
   width: 100%;
   text-align: left;
