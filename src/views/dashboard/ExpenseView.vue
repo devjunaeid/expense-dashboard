07b2ac2bbd3bcbdf4f0676e9416c5f73a7e7339c
junaeid <junaeid.md48@gmail.com>
2024-07-08 00:33:18 +0600

    Added Expense view components.

--- src/views/dashboard/ExpenseView.vue
+++ src/views/dashboard/ExpenseView.vue
@@ -1,13 +1,31 @@
 <script setup>
-  
+import Tips from "@/components/Expense/Tips.vue"
+import MonthlyOverallSpanding from "@/components/Expense/MontlyOverallSpanding.vue"
+import DailyExpense from "@/components/Expense/DailyExpense.vue"
+import ExpenseOverview from "@/components/Expense/ExpenseOverview.vue" 
 </script>
 
 <template>
-  <div>
-    From Expense View.   
+  <div class="w-full h-full flex rounded-2xl">
+
+    <!-- Left Section -->
+    <div class="w-2/3 h-full grid grid-flow-row grid-cols-1 grid-rows-3 px-36 py-12">
+      <div class="flex">
+        <ExpenseOverview />
+      </div>
+      <div class="row-span-2">
+        <DailyExpense />
+      </div>
+    </div>
+
+    <!-- Right Section -->
+    <div class="w-1/3 h-full grid grid-flow-row grid-cols-1 grid-rows-2 p-12 rounded-2xl bg-dim1White">
+      <div>
+        <MonthlyOverallSpanding />
+      </div>
+      <div class="h-full flex flex-col justify-end items-center">
+        <Tips />
+      </div>
+    </div>
   </div>
 </template>
-
-<style scoped>
-  
-</style>
