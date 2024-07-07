9042992eed10616b02ba74b5b019c7d6e3dfa3ee
junaeid <junaeid.md48@gmail.com>
2024-07-08 00:33:57 +0600

    Added custom color and fonts.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'], 
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-   fontFamily:{
-    'poppins': ['Poppins', 'sans-serif'],
-   } 
+    extend: {
+      colors: {
+        dim1White: "#F9FAFC",
+        lightslate: "#979797",
+        semiblue: "#273240", 
+        lightblue: "#EDF0F6",
+       darkblue: "#262A41", 
+        bluegreen: "#404852",
+      },
+      gridTemplateColumns:{
+        '25': 'repeat(25, minmax(0, 1fr))',
+      },
+    },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
   },
   plugins: [],
-}
-
+};
